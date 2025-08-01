0a16c548ce6b7e503357682dbb43deffd26c1277
aquilesaugusto1 <elenaofteste@gmail.com>
2025-08-01 14:49:50 -0300

    Tela de erro

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Manutenção Programada</title>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Inter', sans-serif;
+        background-color: #f7fafc; /* Um fundo bem claro */
+        color: #2d3748; /* Um texto um pouco mais escuro */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        text-align: center;
+    }
+
+    .container {
+        padding: 2rem;
+        background-color: #fff;
+        border-radius: 0.5rem;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px -1px rgba(0, 0, 0, 0.1);
+    }
+
+    .logo-container {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 1.5rem;
+    }
+
+    .logo {
+        max-width: 100px; /* Ajuste o tamanho conforme necessário */
+        height: auto;
+    }
+
+    h1 {
+        font-size: 1.75rem;
+        font-weight: 700;
+        margin-top: 0;
+        margin-bottom: 0.75rem;
+        color: #4a5568;
+    }
+
+    p {
+        font-size: 1rem;
+        color: #718096;
+        max-width: 400px;
+        line-height: 1.5;
+        margin: 0 auto;
+    }
+</style>
+</head>
+<body>
+<div class="container">
+<div class="logo-container">
+<img src="{{ asset('favicon.webp') }}" alt="Logo ProgMUD" class="logo">
+</div>
+<h1>Estamos fazendo uma pequena pausa.</h1>
+<p>Voltaremos em breve com melhorias para você. Agradecemos a sua paciência!</p>
+</div>
+</body>
+</html>
