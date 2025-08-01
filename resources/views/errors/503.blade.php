7519379210f40a740787194acc068802355adcdc
aquilesaugusto1 <elenaofteste@gmail.com>
2025-08-01 15:03:37 -0300

    Adiciona favicon.webp à pasta de imagens

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,6 +5,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Manutenção Programada</title>
+<link rel="icon" href="{{ asset('images/favicon.webp') }}" type="image/webp">
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
 
